af51e9132108377b50e8a8878d3b49a5897b545a
Faheem Pervez <trippin1@gmail.com>
2022-05-04 11:10:49 +0100

    Update autosubsync

--- scripts/autosubsync/main.lua
+++ scripts/autosubsync/main.lua
@@ -1,6 +1,6 @@
 local function lazy_load()
     mp.remove_key_binding("autosubsync-menu")
-    require('autosubsync')
+require('autosubsync')
     mp.command("script_binding autosubsync/autosubsync-menu")
 end
 mp.add_key_binding("n", "autosubsync-menu", lazy_load)
