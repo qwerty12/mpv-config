3d69b34408943aa3af79560af0310c7960375277
Faheem Pervez <trippin1@gmail.com>
2022-05-05 15:28:21 +0100

    osc: inc. deadzone, show more nearer bottom, round corners

--- scripts/uosc/osc.lua
+++ scripts/uosc/osc.lua
@@ -39,7 +39,7 @@ local user_opts = {
     seekbarstyle = "bar",       -- bar, diamond or knob
     seekbarhandlesize = 0.6,    -- size ratio of the diamond and knob handle
     seekrangestyle = "inverted",-- bar, line, slider, inverted or none
-    seekrangeseparate = true,   -- wether the seekranges overlay on the bar-style seekbar
+    seekrangeseparate = true,   -- whether the seekranges overlay on the bar-style seekbar
     seekrangealpha = 200,       -- transparency of seekranges
     seekbarkeyframes = true,    -- use keyframes when dragging the seekbar
     title = "${media-title}",   -- string compatible with property-expansion
@@ -1250,7 +1250,7 @@ layouts["box"] = function ()
     local osc_geo = {
         w = 1100,    -- width
         h = 65,    -- height
-        r = 0,     -- corner-radius
+        r = 10,     -- corner-radius
         p = 0,     -- padding
     }
 
@@ -2979,7 +2979,7 @@ function visibility_mode(mode, no_osd)
     end
 
     -- Reset the input state on a mode change. The input state will be
-    -- recalcuated on the next render cycle, except in 'never' mode where it
+    -- recalculated on the next render cycle, except in 'never' mode where it
     -- will just stay disabled.
     mp.disable_key_bindings("input")
     mp.disable_key_bindings("window-controls")
