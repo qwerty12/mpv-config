cf6219c8665f2a73dc8221ecd426620e2071912b
Faheem Pervez <trippin1@gmail.com>
2022-05-09 15:39:24 +0100

    reload: load when ffmpeg's reconnect settings aren't set

--- scripts/reload/main.lua
+++ scripts/reload/main.lua
@@ -1,8 +1,8 @@
 local function on_file_loaded(event)
-    if mp.get_property("stream-lavf-o", ""):find("reconnect=1") ~= nil then
+    if mp.get_property_bool("demuxer-via-network") and mp.get_property("stream-lavf-o", ""):find("reconnect=1") == nil then
         mp.unregister_event(on_file_loaded)
         require('reload')
     end
 end
 
-mp.register_event("start-file", on_file_loaded)
+mp.register_event("file-loaded", on_file_loaded)
