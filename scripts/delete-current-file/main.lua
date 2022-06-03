f43eba0300ce6518b8f9118420ed47b0c6770e63
Faheem Pervez <trippin1@gmail.com>
2022-06-03 10:57:37 +0100

    Make `delete-current-file` lazy-load work again

--- scripts/delete-current-file/main.lua
+++ scripts/delete-current-file/main.lua
@@ -1,6 +1,6 @@
 local function lazy_load(...)
     mp.unregister_script_message("delete_file")
-    require('src/delete-current-file')
+    require('delete-current-file')
     mp.commandv("script-message-to", "delete_current_file", "delete_file", unpack({...}))
 end
 mp.register_script_message("delete_file", lazy_load)
