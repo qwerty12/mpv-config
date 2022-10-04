65d2bd0e16198373d8ad74a1c8c63bc16a42de1f
Faheem Pervez <trippin1@gmail.com>
2022-10-04 11:44:26 +0100

    disable_sysmenu_in_fullscreen: listen for `video-reconfig`
    Should catch more cases

--- scripts/disable_sysmenu_in_fullscreen.lua
+++ scripts/disable_sysmenu_in_fullscreen.lua
@@ -11,7 +11,7 @@ void __stdcall Sleep(unsigned long dwMilliseconds);
 local GWL_STYLE = -16
 local WS_SYSMENU = 0x00080000
 
-local mpv_hwnd
+local mpv_hwnd = nil
 local function get_mpv_hwnd()
     local hwnd = nil
     local our_pid = mp.get_property_number("pid")
@@ -30,18 +30,22 @@ local function get_mpv_hwnd()
     return hwnd
 end
 
-local function on_fs_change(name, value)
+local function on_fs_change(_, value)
+    value = value ~= nil and value or mp.get_property_bool("fullscreen")
     if not value then return end
 
-    local style
+    local ws_style = nil
     for _ = 1, 20 do
         ffi.C.Sleep(50)
-        style = ffi.C.GetWindowLongPtrW(mpv_hwnd, GWL_STYLE)
-        if bit.band(style, WS_SYSMENU) == WS_SYSMENU then break end
+        local s = ffi.C.GetWindowLongPtrW(mpv_hwnd, GWL_STYLE)
+        if bit.band(s, WS_SYSMENU) == WS_SYSMENU then
+            ws_style = s
+            break
+        end
     end
-    --if mp.get_property_bool(name) then
-        ffi.C.SetWindowLongPtrW(mpv_hwnd, GWL_STYLE, bit.band(style, bit.bnot(WS_SYSMENU)))
-    --end
+    if ws_style and mp.get_property_bool("fullscreen") then
+        ffi.C.SetWindowLongPtrW(mpv_hwnd, GWL_STYLE, bit.band(ws_style, bit.bnot(WS_SYSMENU)))
+    end
 end
 
 local function on_focused(_, value)
@@ -51,7 +55,8 @@ local function on_focused(_, value)
     mpv_hwnd = get_mpv_hwnd()
     if mpv_hwnd ~= nil then
         mp.observe_property("fullscreen", "bool", on_fs_change)
+        mp.register_event("video-reconfig", on_fs_change)
     end
 end
 
-mp.observe_property("focused", "bool", on_focused)
+mp.observe_property("vo-configured", "bool", on_focused)
