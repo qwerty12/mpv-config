540407506f32afa930d0f9390be2347efb81c60e
Faheem Pervez <trippin1@gmail.com>
2022-10-27 18:20:39 +0100

    Update uosc from upstream

--- scripts/uosc/main.lua
+++ scripts/uosc/main.lua
@@ -1,6 +1,5 @@
 if mp.get_property('osc') == 'yes' then
 	return
 end
-local script_dir = mp.get_script_directory()
-package.path = package.path .. ";" .. script_dir .. "\\scripts\\uosc\\?.lua;"
-require('scripts/uosc/main')
+package.path = package.path .. ";" .. mp.get_script_directory() .. "/scripts/?.lua;"
+require('uosc')
