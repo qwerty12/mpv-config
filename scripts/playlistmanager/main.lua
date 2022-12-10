de6715e369f0da1ae2c5b9b695c2b9cea762b442
Faheem Pervez <trippin1@gmail.com>
2022-12-10 10:39:10 +0000

    Use playlistmanager for autoload
    It doesn't spawn PowerShell(!) on startup any more...

--- scripts/playlistmanager/main.lua
+++ scripts/playlistmanager/main.lua
@@ -1,6 +1,1 @@
-local function lazy_load(...)
-    mp.unregister_script_message("playlistmanager")
-    require("playlistmanager")
-    mp.commandv("script-message-to", "playlistmanager", "playlistmanager", unpack({...}))
-end
-mp.register_script_message("playlistmanager", lazy_load)
+require("playlistmanager")
